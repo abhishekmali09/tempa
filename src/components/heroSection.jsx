0507dcb0523820f37803680d690b6dc2d81c1a67
abhishekmali09 <abhishekmali7710@gmail.com>
2025-03-16 00:29:20 +0530

    name

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
         <div className='flex flex-col gap-4 sm:max-w-screen w-1/2 justify-center items-center'>
             <p>Quality Care</p>
             <div className='text-6xl pb-6'>
-                <h1>Your Health,</h1>
+                <h1>Abhishek Health,</h1>
                 <h1>Our Priority</h1>
             </div>
             <button className='cursor-pointer bg-[#966b54] rounded-3xl px-5 py-2 text-white'>Get Medical opinion</button>
